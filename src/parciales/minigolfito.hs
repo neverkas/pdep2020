a95f7fa9894ef09b8879f5298e040e3df408e3dc
jelou <emmanuel.lazarte.012@gmail.com>
2020-05-30 13:43:44 -0300

    Se arreglo la funcion dada mayorSegun Se agrego nueva funcion basica elCuadradoDe Se mejoro la abstraccion del Obstaculo Se corrigio la abstraccion del palo hierro (el enunciado aparenta una contradiccion)

--- src/parciales/minigolfito.hs
+++ src/parciales/minigolfito.hs
@@ -13,8 +13,9 @@ tunelConRampita = ((\(_,p,a) -> p>90 && a==0), (\(v,_,a) -> (v*2,100,0)) )
 hoyo = ((\(v,p,a) -> between 5 20 v && p>95 && a==0), (\ _ -> (0,0,0)))
 between n m x = elem x [n .. m]
 maximoSegun f = foldl1 (mayorSegun f)
-mayorSegun f a b | f a >= f b = a
-| otherwise = b
+mayorSegun f a b
+ | f a >= f b = a
+ | otherwise = b
 
 palos = putter : madera : map hierro [1 .. 10]
 
@@ -33,6 +34,9 @@ elDobleDe = (*2)
 laMitadDe :: Int -> Int
 laMitadDe = flip $ div 2
 
+elCuadradoDe :: Int -> Int
+elCuadradoDe unNumero = unNumero*unNumero
+
 -- PALOS
 type Palo = Habilidad -> Tiro
 putter :: Palo
@@ -42,17 +46,21 @@ madera :: Palo
 madera (fuerza, precision) = (100, laMitadDe precision, 5)
 
 type NumeroHierro = Int
-hierros :: NumeroHierro -> Palo
-hierros numeroHierro (fuerza, precision) = (fuerza*numeroHierro, div precision numeroHierro, numeroHierro*numeroHierro)
-
+hierro :: NumeroHierro -> Palo
+hierro numeroHierro (fuerza, precision) =
+  (fuerza*numeroHierro, div precision numeroHierro, elCuadradoDe numeroHierro)
 
 -- PUNTO 2
 type Nombre = String
 type NombreDelPadre = String
 type Persona = (Nombre, NombreDelPadre, Habilidad)
-type Obstaculo = (Tiro->Bool, Tiro->Tiro)
-condicionObstaculo (condicion, _) = condicion
-efectoObstaculo (_, efecto) = efecto
+
+type CondicionSobreElTiro = Tiro->Bool
+type EfectoSobreElTiro = Tiro->Tiro
+
+type Obstaculo = (CondicionSobreElTiro, EfectoSobreElTiro)
+elTiroSuperaElObstaculo (condicion, _) = condicion
+efectoSobreElTiro (_, efecto) = efecto
 
 golpe :: Persona -> Palo -> Tiro
 golpe unaPersona unPalo =
