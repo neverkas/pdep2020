23efae837fa265b2aae88944f3b709f80f7eb23d
jelou <emmanuel.lazarte.012@gmail.com>
2020-06-04 04:18:34 -0300

    -Se agrega la abstraccion de un nivel -Se agrega el elemento concentracion que imita a meditar tantas veces -Se define la abstraccion de un esbirro -Se genera poder crear esbirros malvados un numero de veces -Se crea abstraccion del personaje jack, y de su katana magica

--- src/parciales/samuraiJack.hs
+++ src/parciales/samuraiJack.hs
@@ -78,3 +78,48 @@ casiLoMataA personaje = algunoEsMortiferoCon personaje.elementos
 algunoEsMortiferoCon :: Personaje -> [Elemento] -> Bool
 algunoEsMortiferoCon personaje = any ((==0).danioQueProduce personaje)
 
+--
+-- # PUNTO 3
+--
+
+-- 3a Definir concentracion de modo que se pueda obtener un elemento cuyo
+-- efecto defensivo sea aplicar meditar tantas veces como el nivel de
+-- concentración indicado y cuyo tipo sea "Magia".
+type Nivel = Int
+
+concentracion :: Nivel -> Elemento
+concentracion nivel = UnElemento "Magia" (meditarTantasVecesComo nivel) ()
+
+meditarTantasVecesComo :: Nivel -> Transformacion
+meditarTantasVecesComo nivel personaje
+  | nivel >= 0 = meditarTantasVecesComo (nivel-1) (meditar personaje)
+  | otherwise = personaje
+
+--3b Definir esbirrosMalvados que recibe una cantidad y retorna una lista
+-- con esa cantidad de esbirros (que son elementos de tipo “Maldad”
+-- cuyo efecto ofensivo es causar un punto de daño).
+type Esbirro = Elemento
+
+esbirrosMalvados :: Int -> [Esbirro]
+esbirrosMalvados cantidadEsbirros = crearEsbirrosHasta cantidadEsbirros
+
+crearEsbirrosHasta :: Int -> [Esbirro]
+--crearEsbirrosHasta 1 = [unEsbirro]
+crearEsbirrosHasta numeroVeces
+ | numeroVeces > 0 = unEsbirro : crearEsbirrosHasta (numeroVeces-1)
+
+unEsbirro :: Esbirro
+unEsbirro = UnElemento "Maldad" (causarDanio 1) ()
+
+-- 3c. Definir jack de modo que permita obtener un personaje que tiene 300 de salud,
+-- que tiene como elementos concentración nivel 3 y una katana mágica (de tipo "Magia"
+-- cuyo efecto ofensivo es causar 1000 puntos de daño) y vive en el año 200.
+jack :: Personaje
+jack = UnPersonaje "Jack" 300 (concentracion 3) [katanaMagica] 200
+
+katanaMagica :: Elemento
+katanaMagica = UnElemento "Magia" (causarDanio 1000) ()
+
+-- 3d. Definir aku :: Int -> Float -> Personaje que recibe el año en el que vive y
+-- la cantidad de salud con la que debe ser construido. Los elementos que tiene
+-- dependerán en parte de dicho año. Los mismos incluyen:
