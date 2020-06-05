e3138a6ec70d5c34912d9085bf23cce6f6df02c0
jelou <emmanuel.lazarte.012@gmail.com>
2020-06-05 00:09:15 -0300

    -Se hicieron correcciones de azucar sintactico -Se removieron los comentarios del enunciado, hacia dificil la lectura

--- src/parciales/samuraiJack.hs
+++ src/parciales/samuraiJack.hs
@@ -18,6 +18,12 @@ data Personaje = UnPersonaje {
 --
 -- # PUNTO 1
 --
+{-
+- # CORRECCIONES:
+- (1)(2) Se puede mejorar aun mas en una sola.
+-   Evita usar azucar sintactico con { } (lo hace mas vistoso pero no usas conceptos de funcional)
+-   aprovecha y usa orden superior, aplicacion parcial, composicion, etc..
+-}
 
 -- 1a. mandarAlAnio: lleva al personaje al año indicado.
 type Transformacion = Personaje->Personaje
@@ -26,38 +32,48 @@ type Anio = Int
 mandarAlAnio :: Anio -> Transformacion
 mandarAlAnio anioFijado personaje = personaje{ anioPresente = anioFijado}
 
--- 1b. meditar: le agrega la mitad del valor que tiene a la salud del personaje.
+
+-- ######################
+-- ### CORRECCION (1) ###
+-- ######################
+
 meditar :: Transformacion
-meditar personaje = personaje{ salud = salud personaje + (reducirAlaMitad.salud) personaje}
+--meditar personaje = personaje{ salud = salud personaje + (reducirAlaMitad.salud) personaje}
+--meditar personaje = aumentarSaludEn (0.5*salud personaje) $ personaje
+meditar = aumentarSaludEn (/2)
 
-reducirAlaMitad :: Float -> Float
-reducirAlaMitad = (/2)
+aumentarSaludEn :: (Float -> Float) -> Transformacion
+aumentarSaludEn cuantoAumenta (UnPersonaje nombre salud elementos anioPresente)=
+  UnPersonaje nombre ((salud+).cuantoAumenta $ salud)  elementos anioPresente
+--reducirAlaMitad :: Float -> Float
+--reducirAlaMitad = (/2)
 
--- 1c. causarDanio: le baja a un personaje una cantidad de salud dada.
--- Hay que tener en cuenta al modificar la salud de un personaje que ésta nunca puede quedar menor a 0.
 type Danio = Float
 
-causarDanio :: Danio -> Transformacion
-causarDanio cantidadDanio personaje = personaje{ salud = (reducirSaludEn cantidadDanio.salud) personaje}
+-- ######################
+-- ### CORRECCION (2) ###
+-- ######################
 
-reducirSaludEn :: Float -> Float -> Float
+causarDanio :: Danio -> Transformacion
+causarDanio cuantoAfecta = reducirSaludEn (cuantoAfecta-)
+--causarDanio cantidadDanio personaje = personaje{ salud = (reducirSaludEn cantidadDanio.salud) personaje}
+
+reducirSaludEn :: (Float -> Float) -> Transformacion
+reducirSaludEn cuantoReduce (UnPersonaje nombre salud elementos anioPresente)=
+  UnPersonaje nombre (max 0 $ cuantoReduce salud)  elementos anioPresente
 --reducirSaludEn danio salud = max 0 (salud - danio)
-reducirSaludEn danio = max 0.((-) danio)
+--reducirSaludEn danio = max 0.((-) danio)
 
 --
 -- # PUNTO 2
 --
 
--- 2a. esMalvado, que retorna verdadero si alguno de los elementos que tiene el personaje en cuestión es de tipo “Maldad”.
 esMalvado :: Personaje -> Bool
-esMalvado personaje = (algunoEsTipo "Malvado".elementos) personaje
+esMalvado = algunoEsTipo "Malvado".elementos
 
 algunoEsTipo :: String -> [Elemento] -> Bool
-algunoEsTipo tipoElegido = any ((== tipoElegido).tipo)
+algunoEsTipo tipoElegido = any ((==) tipoElegido.tipo)
 
--- 2b. danioQueProduce :: Personaje -> Elemento -> Float
--- que retorne la diferencia entre la salud inicial del personaje y la salud del personaje
--- luego de usar el ataque del elemento sobre él.
 danioQueProduce :: Personaje -> Elemento -> Float
 danioQueProduce personaje elemento =
   salud personaje - (salud.luegoDeAtaqueCon elemento) personaje
@@ -65,9 +81,6 @@ danioQueProduce personaje elemento =
 luegoDeAtaqueCon :: Elemento -> Transformacion
 luegoDeAtaqueCon elemento = ataque elemento
 
--- 2c. enemigosMortales que dado un personaje y una lista de enemigos
--- devuelve la lista de los enemigos que pueden llegar a matarlo con un solo elemento.
--- Esto sucede si luego de aplicar el efecto de ataque del elemento, el personaje queda con salud igual a 0.
 type Enemigo = Personaje
 enemigosMortales :: Personaje -> [Enemigo] -> [Enemigo]
 enemigosMortales personaje = filter (casiLoMataA personaje)
@@ -82,9 +95,6 @@ algunoEsMortiferoCon personaje = any ((==0).danioQueProduce personaje)
 -- # PUNTO 3
 --
 
--- 3a Definir concentracion de modo que se pueda obtener un elemento cuyo
--- efecto defensivo sea aplicar meditar tantas veces como el nivel de
--- concentración indicado y cuyo tipo sea "Magia".
 type Nivel = Int
 
 concentracion :: Nivel -> Elemento
@@ -95,9 +105,6 @@ meditarTantasVecesComo nivel personaje
   | nivel >= 0 = meditarTantasVecesComo (nivel-1) (meditar personaje)
   | otherwise = personaje
 
---3b Definir esbirrosMalvados que recibe una cantidad y retorna una lista
--- con esa cantidad de esbirros (que son elementos de tipo “Maldad”
--- cuyo efecto ofensivo es causar un punto de daño).
 type Esbirro = Elemento
 
 esbirrosMalvados :: Int -> [Esbirro]
@@ -111,24 +118,11 @@ crearEsbirrosHasta numeroVeces
 unEsbirro :: Esbirro
 unEsbirro = UnElemento "Maldad" (causarDanio 1) ()
 
--- 3c. Definir jack de modo que permita obtener un personaje que tiene 300 de salud,
--- que tiene como elementos concentración nivel 3 y una katana mágica (de tipo "Magia"
--- cuyo efecto ofensivo es causar 1000 puntos de daño) y vive en el año 200.
 jack :: Personaje
 jack = UnPersonaje "Jack" 300 [concentracion 3, katanaMagica] 200
 
 katanaMagica :: Elemento
 katanaMagica = UnElemento "Magia" (causarDanio 1000) ()
-
--- 3d. Definir aku :: Int -> Float -> Personaje que recibe el año en el que vive y
--- la cantidad de salud con la que debe ser construido. Los elementos que tiene
--- dependerán en parte de dicho año. Los mismos incluyen:
---   1. Concentración nivel 4
---   2. Tantos esbirros malvados como 100 veces el año en el que se encuentra.
---   3. Un portal al futuro, de tipo “Magia” cuyo ataque es enviar al personaje al futuro
---      (donde el futuro es 2800 años después del año indicado para aku),
---      y su defensa genera un nuevo aku para el año futuro correspondiente que mantenga
---      la salud que tenga el personaje al usar el portal.
 
 type Salud = Float
 
