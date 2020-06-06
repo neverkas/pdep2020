93b6d1bebdd12aca6277285d240539df27b8f97b
jelou <emmanuel.lazarte.012@gmail.com>
2020-06-06 18:00:16 -0300

    -Se agregan datos de enunciado (jugador, habilidad, tiro) -Se desarrollan los tipos de palo -Se crea la accion de que un jugador golpee con un palo -Se agrega el obstaculo el hoyo, tunel con rampita, una laguna

---
+++ src/parciales/minigolfito2020.hs
@@ -0,0 +1,100 @@
+module ParcialMinigolfito2020 where
+
+import Text.Show.Functions
+
+-- # DATOS
+data Jugador = UnJugador {
+  nombre :: String,
+  padre :: String,
+  habilidad :: Habilidad
+} deriving (Eq, Show)
+
+data Habilidad = Habilidad {
+  fuerzaJugador :: Int,
+  precisionJugador :: Int
+} deriving (Eq, Show)
+
+bart = UnJugador "Bart" "Homero" (Habilidad 25 60)
+todd = UnJugador "Todd" "Ned" (Habilidad 15 80)
+rafa = UnJugador "Rafa" "Gorgory" (Habilidad 10 1)
+
+data Tiro = UnTiro {
+  velocidad :: Int,
+  precision :: Int,
+  altura :: Int
+} deriving (Eq, Show)
+
+type Puntos = Int
+
+between n m x = elem x [n .. m]
+
+maximoSegun f = foldl1 (mayorSegun f)
+mayorSegun f a b
+  | f a > f b = a
+  | otherwise = b
+
+-- # PUNTO 1
+type Palo = Habilidad -> Tiro
+
+putter :: Palo
+putter (Habilidad _ precision) = UnTiro 10 (elDobleDe precision) 0
+
+madera :: Palo
+madera (Habilidad _ precision) = UnTiro 100 (laMitadDe precision) 5
+
+hierros :: Int -> Palo
+hierros suNumero (Habilidad fuerza precision) =
+  UnTiro (fuerza*suNumero) (div precision suNumero) (queNoSeaMenorACero suNumero-3)
+
+
+-- Revisar
+palos :: [Palo]
+palos = putter : madera : listaDeHierros
+listaDeHierros = map hierros [1..10]
+
+queNoSeaMenorACero :: Int -> Int
+queNoSeaMenorACero = max 0
+
+elDobleDe :: Int -> Int
+elDobleDe = (*2)
+
+laMitadDe :: Int -> Int
+laMitadDe = flip div 2
+
+-- # PUNTO 2
+
+golpe :: Jugador -> Palo -> Tiro
+golpe porQuien conEstePalo = conEstePalo (habilidad porQuien)
+
+-- # PUNTO 3
+type Efecto = Tiro -> Tiro
+type Condicion = Tiro -> Bool
+
+data Obstaculo = UnObstaculo{
+  condicion :: Bool,
+  efecto :: Tiro
+}
+-- me parece que con data queda mas ordenado..
+--type Obstaculo = Tiro -> (Condicion, Efecto)
+
+tunelConRampita :: Tiro -> Obstaculo
+tunelConRampita unTiro = UnObstaculo{
+  condicion = suAlturaEs (==0) unTiro && suPrecisionEs (>90) unTiro,
+  efecto = (cambiarPrecisionA 100.cambiarVelocidadEn (*2)) unTiro
+}
+
+unaLaguna :: Tiro -> Obstaculo
+unaLaguna suLargoEs unTiro = UnObstaculo{
+  condicion = elTiroEs (>80) unTiro && suAlturaEs (between 1 5) unTiro,
+  efecto = cambiarAlturaA (flip div suLargoEs) unTiro
+}
+
+unHoyo :: Tiro -> Obstaculo
+unHoyo unTiro = UnObstaculo{
+  condicion = elTiroEs (between 5 20) unTiro && suAlturaEs (==0) unTiro && suPrecisionEs (>95) unTiro
+  efecto = (cambiarAlturaA 0.cambiarVelocidadA 0.cambiarPrecisionA 0) unTiro
+}
+
+--tiroLuegoDelObstaculo esteTiro unObstaculo =
+--([alRasDelSuelo, suPrecisionEs (>90)], (cambiarPrecisionA 100.cambiarVelocidadEn (*2)))
+--where alRasDelSuelo = suAlturaEs (==0)
