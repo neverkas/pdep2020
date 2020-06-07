0a9d1482ac87d480a0250f86652bf4f1ce972c10
jelou <emmanuel.lazarte.012@gmail.com>
2020-06-07 02:02:06 -0300

    -Se arreglo poder modelar el obstaculo con data como alternativa

--- src/parciales/minigolfito2020.hs
+++ src/parciales/minigolfito2020.hs
@@ -47,11 +47,9 @@ hierros suNumero (Habilidad fuerza precision) =
   UnTiro (fuerza*suNumero) (div precision suNumero) (queNoSeaMenorACero suNumero-3)
 
 
--- Revisar
---palos :: [Palo]
+palos :: [Palo]
 palos = putter : madera : listaHierros
 listaHierros = map hierros [1..10]
---listaHierros n = (!! n) (map hierros [1..10])
 
 queNoSeaMenorACero :: Int -> Int
 queNoSeaMenorACero = max 0
@@ -65,22 +63,39 @@ laMitadDe = flip div 2
 -- # PUNTO 2
 
 golpe :: Jugador -> Palo -> Tiro
-golpe porQuien conEstePalo = conEstePalo (habilidad porQuien)
+golpe porQuien conEstePalo = (conEstePalo.habilidad) porQuien
+-- se podia mejorar!! usando composicicon, porque.. palo es una funcion, y habilidad tambien
+-- golpe porQuien conEstePalo = conEstePalo (habilidad porQuien)
+
 
 -- # PUNTO 3
-type Efecto = Tiro -> Tiro
 type Condicion = Tiro -> Bool
--- data Obstaculo = UnObstaculo{
---   condicion :: Bool,
---   efecto :: Tiro
--- }
+type Efecto = Tiro -> Tiro
 
 type Obstaculo = Tiro -> (Bool, Tiro)
+--type Obstaculo = Tiro -> (Condicion, Efecto)
 condicion (suCondicion, _) = suCondicion
 efecto (_, suEfecto) = suEfecto
--- me parece que con data queda mas ordenado..
---type Obstaculo = Tiro -> (Condicion, Efecto)
 
+data Obstaculo' = UnObstaculo{
+   condicion' :: Bool,
+   efecto' :: Tiro
+}
+
+-- Obstaculo con DATA
+unHoyo' :: Tiro -> Obstaculo'
+unHoyo' unTiro = UnObstaculo{
+  condicion' = all (\condicion -> condicion unTiro) [compararEntre velocidad (between 5 20), compararEntre altura (==0)],
+  efecto' = cambiarAtributosAcero unTiro
+}
+
+unaLaguna' :: Int -> Tiro -> Obstaculo'
+unaLaguna' suLargoEs unTiro = UnObstaculo{
+  condicion' = all (\condicion -> condicion unTiro) [compararEntre velocidad (>80), compararEntre altura (between 1 5)],
+  efecto' = cambiarAlturaA (divididoPor suLargoEs) unTiro
+}
+
+-- Obstaculo como FUNCION (usando Type)
 tunelConRampita :: Obstaculo
 tunelConRampita unTiro =
   (compararEntre altura (==0) unTiro && compararEntre precision (>90) unTiro,
@@ -97,24 +112,7 @@ unHoyo :: Obstaculo
 unHoyo unTiro =
   (compararEntre velocidad (between 5 20) unTiro && compararEntre altura (==0) unTiro && compararEntre precision (>95) unTiro,
   cambiarAtributosAcero unTiro)
-{-
-unaLaguna :: Int -> Tiro -> Obstaculo
-unaLaguna suLargoEs unTiro = UnObstaculo{
-  -- condicion = laVelocidadEs (>80) unTiro && suAlturaEs (between 1 5) unTiro,
-  condicion = compararEntre velocidad (>80) unTiro && compararEntre altura (between 1 5) unTiro,
-  -- se podia mejorar la expresividad
-  -- efecto = cambiarAlturaA (flip div suLargoEs) unTiro
-  efecto = cambiarAlturaA (divididoPor suLargoEs) unTiro
-}
 
-unHoyo :: Tiro -> Obstaculo
-unHoyo unTiro = UnObstaculo{
-  -- condicion = elTiroEs (between 5 20) unTiro && suAlturaEs (==0) unTiro && suPrecisionEs (>95) unTiro,
-  condicion = compararEntre velocidad (between 5 20) unTiro && compararEntre altura (==0) unTiro && compararEntre precision (>95) unTiro,
-  -- efecto = (cambiarAlturaA (*0).cambiarVelocidadA (*0).cambiarPrecisionA (*0)) unTiro
-  efecto = cambiarAtributosAcero unTiro
-}
--}
 tiroLuegoDelObstaculo :: Tiro -> Obstaculo -> Tiro
 tiroLuegoDelObstaculo esteTiro unObstaculo
   | superaElObstaculo = producirEfecto
@@ -126,6 +124,7 @@ tiroLuegoDelObstaculo esteTiro unObstaculo
 -- cambiarA :: Atributo -> (Int->Int) -> Efecto
 -- cambiarA suAtributo porCuanto esteTiro = (porCuanto.suAtributo) esteTiro
 
+-- Es mas claro y preciso si dice "tiro detenido"
 cambiarAtributosAcero :: Efecto
 cambiarAtributosAcero unTiro = (cambiarAlturaA (*0).cambiarVelocidadA (*0).cambiarPrecisionA (*0)) unTiro
 
@@ -159,15 +158,24 @@ palosUtiles conEsteJugador esteObstaculo =
   filter (esUtilCon esteObstaculo.golpe conEsteJugador) palos
 
 esUtilCon :: Obstaculo -> Tiro -> Bool
-esUtilCon esteObstaculo esteTiro = condicion (esteObstaculo esteTiro)
+--esUtilCon esteObstaculo esteTiro = condicion (esteObstaculo esteTiro)
+-- de esta manera utilizo composicicion
+esUtilCon esteObstaculo esteTiro = (condicion.esteObstaculo) esteTiro
 
+-- Ojo..!! No esta bien, deberias resolverlo con takeWhile y fold..
+-- Porque pide que sean tiros consecutivos, que se vayan modificando al pasar por cada obstaculo..
 cuantosObstaculosSupera :: [Obstaculo] -> Tiro -> Int
 cuantosObstaculosSupera variosObstaculos esteTiro =
-  (contarCuantos.takeWhile (esSuperadoPor esteTiro)) variosObstaculos
+  -- (contarCuantos.takeWhile (flip esUtilCon esteTiro)) variosObstaculos
+  -- estabas repitiendo logica, ya existia una funcion esUtilCon que hacia lo mismo
+   (contarCuantos.takeWhile (esSuperadoPor esteTiro)) variosObstaculos
   --(contarCuantos.filter (esSuperadoPor esteTiro)) variosObstaculos
 
+-- estabas repitiendo logica, ya existia una funcion esUtilCon que hacia lo mismo
 esSuperadoPor :: Tiro -> Obstaculo -> Bool
 esSuperadoPor esteTiro unObstaculo = condicion (unObstaculo esteTiro)
+-- de esta manera utilizo composicion
+-- esSuperadoPor esteTiro unObstaculo = (condicion.unObstaculo) esteTiro
 
 contarCuantos :: [a] -> Int
 contarCuantos deEstaLista = length deEstaLista
@@ -179,10 +187,23 @@ paloMasUtil esteJugador variosObstaculos =
 
 cuantosSupera :: Jugador -> [Obstaculo] -> Palo -> Int
 cuantosSupera jugador obstaculos = cuantosObstaculosSupera obstaculos.golpe jugador
-
 -- el enunciado ya te daba una funcion..
--- cualSuperaMas :: Jugador -> [Obstaculo] -> Palo -> Palo -> Palo
+--cualSuperaMas :: Jugador -> [Obstaculo] -> Palo -> Palo -> Palo
 -- cualSuperaMas jugador obstaculos paloA paloB
---   | cuantosSupera paloA > cuantosSupera paloB = paloA
+ --  | cuantosSupera paloA > cuantosSupera paloB = paloA
 --   | otherwise = paloB
 --   where cuantosSupera = cuantosObstaculosSupera obstaculos.golpe jugador
+
+-- # PUNTO 5
+type Padre = String
+type InfoDelJugador = (Jugador, Puntos)
+--susPuntos (_, puntos) = puntos
+elJugador = fst
+susPuntos = snd
+
+--losPadresQuePerdieronApuesta :: [InfoDelJugador] -> [Padre]
+-- losPadresQuePerdieronApuesta variosNinios =
+--   (losPadresSon.filter (mayorSegun susPuntos)) variosNinios
+
+-- losPadresSon :: [InfoDelJugador] -> [String]
+-- losPadresSon deEstosNinios = map (padre.fst) deEstosNinios
