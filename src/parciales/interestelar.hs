a0e2933e75e2fcdb1a2efc49fbe2488a68944baf
jelou <emmanuel.lazarte.012@gmail.com>
2020-06-07 23:54:08 -0300

    -Se agrega poder ver las funciones -Se mejora abstraccion del viajero -Se mejora el como viajar en una nave -Se incorpora un viaje rescatista -Se agregan simulaciones del progama

--- src/parciales/interestelar.hs
+++ src/parciales/interestelar.hs
@@ -1,7 +1,9 @@
 module ParcialInterestelar where
 
+import Text.Show.Functions
+
 -- DATOS
-data Planeta = UnPlaneta String Posicion (Int->Int)
+data Planeta = UnPlaneta String Posicion (Int->Int) deriving(Show)
 type Posicion = (Float, Float, Float)
 posicion (UnPlaneta _ p _) = p
 tiempo (UnPlaneta _ _ t) = t
@@ -9,7 +11,7 @@ coordX (x,_,_) = x
 coordY (_,y,_) = y
 coordZ (_,_,z) = z
 
-data Astronauta = UnAstronauta String Int Planeta
+data Astronauta = UnAstronauta String Int Planeta deriving(Show)
 nombre (UnAstronauta n _ _) = n
 edad (UnAstronauta _ e _) = e
 planeta (UnAstronauta _ _ p) = p
@@ -44,9 +46,9 @@ tiempoEnViajarA velocidad origen destino = (flip div velocidad.distanciaEntre or
 --
 --  # PUNTO 2
 --
-type Viajo = Astronauta->Astronauta
+type Viajero = Astronauta->Astronauta
 
-pasarTiempoEn :: Planeta -> Viajo
+pasarTiempoEn :: Planeta -> Viajero
 pasarTiempoEn unPlaneta astronauta = cambiarEdad ((+) (aniosIndicados unPlaneta astronauta)) astronauta
 -- esto rompia porque... tiempo de planeta pedia tambien la edad del astronauta
 -- pasarTiempoEn unPlaneta astronauta = (cambiarEdad ((+) tiempo unPlaneta)) astronauta
@@ -57,7 +59,7 @@ aniosIndicados planeta astronauta = (tiempo planeta.edad) astronauta
 -- esta otra forma no usa composicion
 --aniosIndicados planeta astronauta = tiempo planeta (edad astronauta)
 
-cambiarEdad :: (Int->Int) -> Viajo
+cambiarEdad :: (Int->Int) -> Viajero
 cambiarEdad cuantosAnios (UnAstronauta nombre edad planeta) =
   UnAstronauta nombre (cuantosAnios edad) planeta
 -- es mejor pasarle una funcion que recibe una operacion (*,+,-,/) para generalizar y evita repiticion de codigo
@@ -78,10 +80,13 @@ naveFuturista _ _ = 0
 -- no era necesario calcular nada..
 -- naveFuturista = tiempoEnViajarA 1
 
-viajarEn :: Nave -> Planeta -> Planeta -> Viajo
-viajarEn nave origen destino astronauta =
-  (cambiarPlanetaA destino.cambiarEdad ((+) tiempoDeViaje)) astronauta
-  where tiempoDeViaje = nave origen destino
+viajarEn :: Nave -> Planeta -> Viajero
+-- viajarEn :: Nave -> Planeta -> Planeta -> Viajero
+viajarEn nave destino astronauta =
+-- no era necesario poner el destino, porque se saca del astronauta
+-- viajarEn nave origen destino astronauta =
+  (cambiarPlanetaA destino.cambiarEdad (+tiempoDeViaje)) astronauta
+  where tiempoDeViaje = nave (planeta astronauta) destino
 
 cambiarPlanetaA :: Planeta -> Astronauta -> Astronauta
 cambiarPlanetaA nuevoPlaneta (UnAstronauta nombre edad planeta) = UnAstronauta nombre edad nuevoPlaneta
@@ -89,3 +94,40 @@ cambiarPlanetaA nuevoPlaneta (UnAstronauta nombre edad planeta) = UnAstronauta n
 --
 -- # Punto 4
 --
+
+type Rescate = [Astronauta] -> Nave -> Planeta -> [Astronauta]
+rescateA :: Astronauta -> Rescate
+rescateA astronauta tripulacion nave destino =
+  (viajeDeRescateDeRegresoEn nave . incluirAlaTripulacionA astronautaVarado . viajarADestino) tripulacion
+   where astronautaVarado = astronautaVaradoEn destino nave astronauta
+         viajarADestino = viajeDeTripulacionEn nave destino
+
+incluirAlaTripulacionA :: Astronauta -> [Astronauta] -> [Astronauta]
+incluirAlaTripulacionA astronauta tripulacion = astronauta : tripulacion
+
+astronautaVaradoEn :: Planeta-> Nave -> Astronauta -> Astronauta
+astronautaVaradoEn unPlaneta naveDeRescate astronauta =
+  (pasarTiempoEn unPlaneta) astronauta
+
+viajeDeTripulacionEn :: Nave -> Planeta -> [Astronauta] -> [Astronauta]
+viajeDeTripulacionEn nave destino = map (viajarEn nave destino)
+
+viajeDeRescateDeRegresoEn :: Nave -> [Astronauta] -> [Astronauta]
+viajeDeRescateDeRegresoEn nave = map (\tripulante -> viajarEn nave (planeta tripulante) tripulante)
+
+-- -----------------------------------------------------------------------------------------------------------
+
+
+startage = naveVieja 3
+
+pluton = UnPlaneta "Pluton" (10,10,10) (id)
+jupiter = UnPlaneta "Jupiter" (50,10,10) (id)
+
+carlitos = UnAstronauta "Carlitos" 33 pluton
+pedrito = UnAstronauta "Pedrito" 33 pluton
+tripulacion = replicate 5 (UnAstronauta "Rescatista" 25 pluton)
+
+-- pruebas
+quedarseEnPluton = pasarTiempoEn pluton carlitos
+viajeLoco = viajarEn (naveVieja 3) jupiter carlitos
+simulacionDeRescate = rescateA carlitos tripulacion startage jupiter
