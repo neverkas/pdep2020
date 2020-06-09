1ea115e56e9d48e1b9fd1b5af1c5982c53072da6
jelou <emmanuel.lazarte.012@gmail.com>
2020-06-09 02:12:21 -0300

    -Se cambio la abstraccion de ejercicio -Se agrega que pueda hacer los ejercicios en cinta, con pesas, en colina, montania

--- src/parciales/aquemaresasgrasitas.hs
+++ src/parciales/aquemaresasgrasitas.hs
@@ -6,9 +6,11 @@ type Edad = Int
 type Peso = Int
 type Tonificacion = Int
 type Minutos = Int
-type Ejercicio = (Persona -> Minutos) -> Persona
+type Ejercicio = Minutos -> Persona -> Persona
 
 type Persona = (Edad, Peso, Tonificacion)
+
+type Velocidad = Int
 
 edad :: Persona -> Int
 peso :: Persona -> Int
@@ -41,9 +43,10 @@ estaObeso persona = ((>100) . peso) persona
 -- # Punto 2
 --
 
--- 150kcal -> 1kg && 300kcal -> x  = 300kcal/150kcal= bajara 2kg
-quemarCalorias :: Persona -> Int -> Persona
-quemarCalorias persona cuantasCalorias
+-- 150kcal -> 1kg
+-- 300kcal -> x  = 300kcal/150kcal= bajara 2kg
+quemarCalorias :: Int -> Persona -> Persona
+quemarCalorias cuantasCalorias persona
   | estaObeso persona = cambiarPesoEn (restarle pesoPerdidoEstandoObeso) persona
   | esMayorDe30 && noEstaObeso && cuantasCalorias > 200 = cambiarPesoEn (restarle 1) persona
   | otherwise = cambiarPesoEn (restarle pesoEnRelacionCaloriasPesoEdad) persona
@@ -62,4 +65,37 @@ cambiarPesoEn cuantoCambiar (edad, peso, tonificacion) = (edad, cuantoCambiar pe
 
 --
 -- # Punto 3
+--
+-- type Ejercicio = Persona -> Minutos -> Persona
+
+-- kmPorHoraAMetrosPorSegundo cuantosKm = div (cuantosKm*1000) 3600
+velocidadPromedio :: Int -> Int -> Int
+velocidadPromedio velocidad tiempo = tiempo*velocidad
+
+velocidadPromedioAcelerado :: Int -> Int -> Int
+velocidadPromedioAcelerado velocidad tiempo = tiempo*(velocidad + div 14 2)
+
+cambiarTonificacionEn :: (Int->Int) -> Persona -> Persona
+cambiarTonificacionEn cuantoCambiar (edad, peso, tonificacion) = (edad, peso, cuantoCambiar tonificacion)
+
+caminata :: Ejercicio
+caminata minutos persona = quemarCalorias (velocidadPromedio 5 minutos) persona
+entrenamientoEnCinta :: Ejercicio
+entrenamientoEnCinta minutos persona = quemarCalorias (velocidadPromedioAcelerado 6 minutos) persona
+
+pesas :: Int -> Ejercicio
+pesas kilos minutos persona
+  | minutos > 10 = cambiarTonificacionEn ((+) $ div kilos 10) persona
+  | otherwise = persona
+
+
+colina :: Int -> Ejercicio
+colina inclinacion minutos persona = quemarCalorias (2*minutos*inclinacion) persona
+
+montania :: Int -> Ejercicio
+montania inclinacion minutos persona =
+  (cambiarTonificacionEn (+1) .colina (inclinacion+3) (div minutos 2).colina inclinacion (div minutos 2)) persona
+
+--
+-- # Punto 4
 --
